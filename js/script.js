8ce13b4e88b523996df48ace8de5cd567ae384f7
Nicolò <nicocriva1@gmail.com>
2021-06-23 15:15:55 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Descrizione: Rifare l'esercizio della to do list come fatto assieme in classe:
+// - stampare in pagina un item per ogni elemento contenuto in un array
+// - ogni item ha una "x" associata: cliccando su di essa, l'item viene rimosso dalla lista
+// - predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista
